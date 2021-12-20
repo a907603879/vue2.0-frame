053721d1e53ea1816d37e8ce44771477045c6f76
a907603879 <a907603879@163.com>
2021-12-20 11:53:16 +0800

    init

---
+++ src/utils/clipboard.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+
+function clipboardSuccess() {
+  Vue.prototype.$message({
+    message: 'Copy successfully',
+    type: 'success',
+    duration: 1500
+  })
+}
+
+function clipboardError() {
+  Vue.prototype.$message({
+    message: 'Copy failed',
+    type: 'error'
+  })
+}
